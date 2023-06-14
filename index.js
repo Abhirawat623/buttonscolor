7653d1274b402a72719afa053c080a393c8ef533
abhirawat623 <abhirawat623@gmail.com>
2023-06-14 17:42:08 +0530

    done

---
+++ index.js
@@ -0,0 +1,28 @@
+let allButtons = document.querySelector("#buttons");
+
+allButtons.addEventListener("click",(event)=>{
+
+    // console.log(event.target.innerText)
+    let btnText = event.target.innerText;
+
+    if(btnText==="Red"){
+    event.target.classList.toggle("btn-red");
+    }
+    else if(btnText==="Yellow"){
+        event.target.classList.toggle("btn-yellow");
+    }
+
+    else if(btnText==="Blue"){
+        event.target.classList.toggle("btn-blue");
+        }
+
+
+        else if(btnText==="Green"){
+            event.target.classList.toggle("btn-green");
+     }
+
+     else if(btnText==="Pink"){
+        event.target.classList.toggle("btn-pink");
+        }
+
+})
